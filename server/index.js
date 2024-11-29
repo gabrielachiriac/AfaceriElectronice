d65a7b0c50c1a58a6de0e630a50c035c687b89f4
Lungu Mihai Adrian <lungu.snk199@gmail.com>
2024-11-29 19:32:07 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
